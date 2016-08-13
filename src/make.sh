7d206b7b801aafcffb65b5c128808eb61ba16ee2
Leon Bogaert <leon@tim-online.nl>
2016-08-14 01:58:05 +0200

    First go 1.4 snap

--- src/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-unset GOROOT
-export GOROOT=`realpath ../install`
-export GOROOT_BOOTSTRAP=`realpath ../../go14/build/`
-export GOROOT_FINAL="/"
-export CGO_ENABLED=0
-cd src
-./make.bash
